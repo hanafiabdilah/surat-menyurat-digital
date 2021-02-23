209478afd40f3b3b54d93779ddc219f717466722
hanafabdilah <handilah81@gmail.com>
2021-02-23 18:34:09 +0700

    Add Migration

---
+++ database/migrations/2021_02_23_035706_create_sifat_surats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSifatSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sifat_surats', function (Blueprint $table) {
+            $table->id();
+            $table->string('sifat_surat');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sifat_surats');
+    }
+}
---
+++ database/migrations/2021_02_23_035717_create_transaksi_surats_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi_surats', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_agenda');
+            $table->string('pengirim');
+            $table->string('no_surat');
+            $table->text('isi_ringkas');
+            $table->date('tanggal_surat');
+            $table->date('tanggal_diterima');
+            $table->text('keterangan')->nullable();
+            $table->text('file')->nullable();
+            $table->string('kategori');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi_surats');
+    }
+}
