0fb0f2037cadfa21ab2a7d0f6ab63be288253083
Muhamad Ichsan <mhmdiksan2121@gmail.com>
2021-02-24 22:18:08 +0700

    menghapus file main di folder layouts

--- resources/views/transaksi_surat/create.blade.php
+++ resources/views/transaksi_surat/create.blade.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 @extends('layouts.app')
 
 @section('title', 'Transaksi Surat | Tambah ')
@@ -136,6 +136,3 @@
                      </div>
     </div>
 @endsection
-=======
-@extends('layouts.app')
->>>>>>> 8a642119028e670fa34692a250b68bc305e7337d
