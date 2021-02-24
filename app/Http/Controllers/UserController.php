468f325fbed19baeb8675838c95998d056e37b9e
hanafabdilah <handilah81@gmail.com>
2021-02-24 07:25:13 +0700

    Membuat Endpoint User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('tes');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cekUsername = User::where('username', $request->username)->count();
+        if ($cekUsername < 1) {
+            $request->request->add(['created_by' => 'admin']);
+            $request['password'] = Hash::make($request->password);
+            User::create($request->all());
+            return back()->with('message', 'User berhasil ditambahkan');
+        }
+        return back()->with('message', 'Username sudah digunakan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('tes', ['user' => $user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $cekUsername = User::where('username', $request->username)->count();
+        if ($cekUsername < 1) {
+            $request['password'] = Hash::make($request->password);
+            $user->update($request->all());
+            return back()->with('message', 'User berhasil diupdate');
+        }
+        return back()->with('message', 'Username sudah digunakan');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return back()->with('message', 'User berhasil dihapus');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nama', 'email', 'role', 'username', 'password', 'created_by',
     ];
 
     /**
